a237e78655850821c00b02d83533192e5a820fd3
sdmkhan123 <sdmkhan126410@gmail.com>
2021-08-14 03:45:59 +0530

    part time emp wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,12 +1,20 @@
 #! /bin/bash
+FullTime=1
+PartTime=2
+NotPresent=0
 Wage_per_Hr=20
 echo "Welcome in Main Branch"
-a=$((RANDOM%2))
+a=$((RANDOM%3))
 if [ $a -eq 1 ]
 then
 	echo "present"
 	empHr=8
 	dailyWage=$(( $Wage_per_Hr * $empHr ))
+elif [ $a -eq 2 ]
+then
+	echo "Part Time Present"
+	empHr=8
+	empWage=$(( $Wage_per_Hr * $empHr))
 else
 	echo "Not Present"
 	empHr=0
