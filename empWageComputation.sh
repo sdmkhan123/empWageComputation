66341ba7dcd95a624f68ac65af510893ea956aad
sdmkhan123 <sdmkhan126410@gmail.com>
2021-08-14 03:28:32 +0530

    dailyWage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
-
+Wage_per_Hr=20
 echo "Welcome in Main Branch"
 a=$((RANDOM%2))
 if [ $a -eq 1 ]
 then
 	echo "present"
+	empHr=8
+	dailyWage=$(( $Wage_per_Hr * $empHr ))
 else
 	echo "Not Present"
+	empHr=0
+	dailyWage=0
 fi
