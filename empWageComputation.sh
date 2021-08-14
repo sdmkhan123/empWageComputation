7ce698813dd64d22509f2c91ca12fe83f41886f1
sdmkhan123 <sdmkhan126410@gmail.com>
2021-08-14 09:34:20 +0530

    Monthly wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,12 +2,22 @@
 
 IS_PRESENT_FULL_TIME=1
 IS_PRESENT_PART_TIME=2
-empCheck=$((RANDOM%2))
-case $empCheck in
-	$IS_PRESENT_FULL_TIME)
-		echo "FUll Time Present";;
-	$IS_PRESENT_PART_TIME)
-		echo "Half Time Present";;
-	*)
-		echo "Not Present";;
-esac
+EMP_RATE_HR=20
+MAX_WORKING_DAYS=20
+totalWorkHr=0
+day=1
+while [ $day -le $MX_WORKING_DAYS ]
+do
+	empCheck=$((RANDOM%3))
+	case $empCheck in
+		$IS_PRESENT_FULL_TIME)
+			empHr=8;;
+		$IS_PRESENT_PART_TIME)
+			empHr=8;;
+		*)
+			empHr=0;;
+	esac
+	totalWorkHr=$(( totalWorkHr + empHr ))
+	((day++))
+done
+MonthlyWage=$(( totalWorkHr * EMP_RATE_HR ))
