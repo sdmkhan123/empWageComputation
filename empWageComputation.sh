2f4357cace74a822fa16416c9a51cf4c0dda0954
sdmkhan123 <sdmkhan126410@gmail.com>
2021-08-14 10:13:19 +0530

    Write a func to get Work hr

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -7,18 +7,27 @@ EMP_RATE_HR=20
 MAX_WORKING_DAYS=20
 totalWorkHr=0
 day=1
+
+function getWorkHr(){
+	local empCheck=$1
+	local empHr=0
+        case $empCheck in
+                $IS_PRESENT_FULL_TIME)
+                        empHr=8;;
+                $IS_PRESENT_PART_TIME)
+                        empHr=8;;
+                *)
+                        empHr=0;;
+        esac
+	echo $empHr
+}
+
 while [ $day -le $MX_WORKING_DAYS ] && [ $totalWorkingHr -lt  $MAX_WORK_HR ]
 do
 	empCheck=$((RANDOM%3))
-	case $empCheck in
-		$IS_PRESENT_FULL_TIME)
-			empHr=8;;
-		$IS_PRESENT_PART_TIME)
-			empHr=8;;
-		*)
-			empHr=0;;
-	esac
+	empHr="$( getWorkHr $empCheck )"
 	totalWorkHr=$(( totalWorkHr + empHr ))
 	((day++))
 done
-MonthlyWage=$(( totalWorkHr * EMP_RATE_HR ))
+# MonthlyWage=$(( totalWorkHr * EMP_RATE_HR ))
+echo $totalWorkHr
